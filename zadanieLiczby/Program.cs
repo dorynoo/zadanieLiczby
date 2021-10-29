7ecf2f7d6f49e03995bb34ade98cd18f63b54e6c
dorynoo <91785835+dorynoo@users.noreply.github.com>
2021-10-29 22:00:20 +0200

    Add project files.

---
+++ zadanieLiczby/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace zadanieLiczby
+{
+    class Program
+    {
+        static bool Czy_jest_liczbą_pierwszą(int liczba)
+        {
+            if (liczba == 2)
+            {
+                return true;
+            }
+            if (liczba <= 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 2; i < liczba; i++)
+                {
+                    if (liczba % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+
+
+
+        }
+
+        static void wypisz_n_liczb_pierwszych(int n)
+        {
+            int licznik = 0;
+            int iterator = 0;
+            Console.WriteLine("zacznij od nowa");
+            while (licznik < n)
+            {
+                //Console.Write(iterator);
+                //Console.WriteLine(Czy_jest_liczbą_pierwszą(iterator));
+
+                if (Czy_jest_liczbą_pierwszą(iterator))
+                {
+                    Console.WriteLine(iterator);
+                    licznik++;
+
+                }
+                iterator++;
+
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            /*   Random rnd = new Random();
+               int wylosowano = rnd.Next(0, 10);
+
+               Console.WriteLine(wylosowano);
+               int liczba = Convert.ToInt32(Console.ReadLine());
+               if (wylosowano == liczba)
+               {
+                   Console.WriteLine("trafiłeś");
+               }
+               else
+               {
+                   Console.WriteLine("jeszcze raz");
+               }
+
+               bool test = Czy_jest_liczbą_pierwszą(liczba);
+               Console.WriteLine(test);
+            */
+            int liczby_pierwsze = Convert.ToInt32(Console.ReadLine());
+            wypisz_n_liczb_pierwszych(liczby_pierwsze);
+        }
+    }
+}
+        
+
+
+                
+
+            
+
+
+        
+        
+    
